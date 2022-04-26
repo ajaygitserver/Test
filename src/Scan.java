8b9444e15410d2228b3cae4d3efac1e2878415f0
ajaygitserver <ajayaj6666@gmail.com>
2022-04-26 16:54:29 +0530

    testtwo

--- src/Scan.java
+++ src/Scan.java
@@ -7,9 +7,17 @@ System.out.println("enter the empid"  );
 int empid=s.nextInt();
 System.out.println(empid);
 	}
+	
+	
+	
+	
+	public void test() {
+		System.out.println("Hai this is test");
+	}
 	public static void main(String[] args) {
 		Scan ss = new Scan();
 		ss.add();
+		ss.test();
 	}
 
 }
